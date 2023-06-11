7f55f7e730aeecda0d09b63fd3cad2f4682fde5a
vstarvrajesh <109358855+vstarvrajesh@users.noreply.github.com>
2023-06-11 17:30:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { AiFillDelete } from "react-icons/ai"
+import { IoMdDoneAll } from 'react-icons/io'
+import { v4 as uuidv4 } from 'uuid'
+uuidv4();
 
 function App() {
+  const [todo, setTodo] = useState([])
+  const [data, setData] = useState({
+    todos: ""
+  })
+
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const addtoarray = () => {
+    if (data.todos !== "") {
+      setData({ ...data, todos: "" })
+      setTodo([...todo, { id: uuidv4(), todo: data.todos, isCompleted: false, isEditing: false }])
+    }
+    document.querySelector('input').value = ""
+  }
+  localStorage.setItem("todo", JSON.stringify(...todo))
+
+  const compelet = (id) => {
+    setTodo(todo.map((a) => a.id === id ? { ...a, isCompleted: !a.isCompleted } : a))
+  }
+  console.log(todo)
+  const deletetodo = (a) => {
+    setTodo(todo.filter(item => item !== a))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>To Do List</h2>
       </header>
-    </div>
+      <div className='inputs'>
+        <input
+          type="text"
+          name='todos'
+          placeholder='Enter your To-Do work'
+          onChange={onChange}
+          required
+        />
+        <button onClick={addtoarray}>Add</button>
+      </div>
+      <div className="todo-list">
+        <div className='todo-wrap'>
+          {
+            todo.map((a) => (
+
+              a.isCompleted ? (
+                <div style={{ background: "#47bc6b" }} className='todo-item' key={a.id}>
+                  <div className='todo-txt'>
+                    <div>{a.todo}</div>
+                    <div className='todo-btn'>
+                      <div onClick={() => compelet(a.id)}>
+                        <IoMdDoneAll />
+                      </div>
+                      <div onClick={() => deletetodo(a)} className='delete'>
+                        <AiFillDelete />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className='todo-item' key={a.id}>
+                  <div className='todo-txt'>
+                    <div >{a.todo}</div>
+                    <div className='todo-btn'>
+                      <div onClick={() => compelet(a.id)}>
+                        <IoMdDoneAll />
+                      </div>
+                      <div onClick={() => deletetodo(a)} className='delete'>
+                        <AiFillDelete />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            ))
+          }
+        </div>
+      </div>
+    </div >
   );
 }
+
 
 export default App;
